94b5ced55b4ef5469c53716e767dfd6222ecde7f
Daudi Mwebaza <david.mwebaza@andela.com>
2018-01-11 18:54:43 +0300

    [CH] Refactor Delete Button Component

--- react-client/src/components/generic/delButton.component.js
+++ react-client/src/components/generic/delButton.component.js
@@ -5,15 +5,16 @@ class ItemDelButton extends Component {
     
     handleClick = (props) => {
         let url = 'http://localhost:5000/shoppinglists/';
-        
+        // if props is not null, generate appropriate url for list or item deletion
         if (this.props) {
             if (this.props.item_id && this.props.list_id)
-            // if props is not null, generate url by appending the list and item id
-                url = url + this.props.list_id + '/' + this.props.item_id
-            
-            // make a call to the api to delete the selected item
-                axios.delete(url, {
-                headers: {'Authorization': localStorage.getItem('token')}
+                url = url + this.props.list_id + '/items/' + this.props.item_id;
+            else if (this.props.list_id && !this.props.item_id)
+                url = url + this.props.list_id;
+
+            // make a call to the api to delete the selected item/list
+            axios.delete(url, {
+            headers: {'Authorization': localStorage.getItem('token')}
             }).then(response => {
                 console.log(response);
             }).catch(error => {
@@ -23,9 +24,10 @@ class ItemDelButton extends Component {
             console.log('Something went wrong!');
         } 
     }
+
     render() {
         return (
-            <button onClick={this.handleClick} ></button>
+            <button onClick={this.handleClick} >Delete Item</button>
         )
     }
 }
