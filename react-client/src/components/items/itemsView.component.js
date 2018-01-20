5c9fbd09b4a2bf5cfc65822a883389567ba4fe5a
Daudi Mwebaza <david.mwebaza@andela.com>
2018-01-20 16:42:04 +0300

    [Feature] Complete logOut functionality and allow it reroute to logIn page on success

--- react-client/src/components/items/itemsView.component.js
+++ react-client/src/components/items/itemsView.component.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Lister from '../generic/list.component';
-import LogOut from '../auth/logOut.component';
+import LogOutwithRouter from '../auth/logOut.component';
 import CreateItem from './createItem.component';
 import axios from 'axios';
 import baseURL from '../generic/base';
@@ -54,7 +54,7 @@ class ItemsView extends Component{
         return (
             <div>
                 <Search setValue={this.setListItems} />
-                <LogOut />
+                <LogOutwithRouter />
                 <CreateItem list_id={this.props.match.params.list_id} callback={this.fetchItems}/>
                 <Lister list={this.state.listItems} callback={this.fetchItems}/>
                 { pagss }
--- react-client/src/components/lists/listView.component.js
+++ react-client/src/components/lists/listView.component.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import baseURL from '../generic/base'
 import Lister from '../generic/list.component';
-import LogOut from '../auth/logOut.component';
+import LogOutwithRouter from '../auth/logOut.component';
 import CreateList from './createList.component';
 import Search from '../generic/search.component';
 
@@ -55,7 +55,7 @@ class ListView extends Component {
         return (
             <div>
                 <Search setValue={this.setLists} />
-                <LogOut />
+                <LogOutwithRouter />
                 <CreateList callback={this.fetchList}/>
                 <p> See all your lists here: </p>
                 <Lister list={this.state.list} callback={this.fetchList} />
