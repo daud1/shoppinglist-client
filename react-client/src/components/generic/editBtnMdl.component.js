af97fac5bf17c5aeb4e27c0d3ab8fa5633e9384d
Daudi Mwebaza <david.mwebaza@andela.com>
2018-02-08 11:53:52 +0300

    [Chore#154351713] Fix PR issues(ES6 compliance)

---
+++ react-client/src/components/generic/editBtnMdl.component.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import baseURL from '../generic/base';
+import axios from 'axios';
+
+class EditBtnMdl extends Component {
+   
+    handleSubmit = (event) => {
+        event.preventDefault();
+        
+        let url = baseURL + 'shoppinglists/';
+        let form = new FormData(event.target);
+
+        if(this.props) {
+            if (this.props.item_id && this.props.list_id){
+                url = url + this.props.list_id + '/items/' + this.props.item_id;
+            } else if (this.props.list_id && !this.props.item_id) {
+                url = url + this.props.list_id;
+            }
+            axios.put(url, form, {
+                headers: {'Authorization': localStorage.getItem('token')}
+            })
+            .then(response => {
+                console.log(response);
+                this.myFormRef.reset();
+                this.props.callback();
+            })
+            .catch(error => {
+                console.log(error);
+            });
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.handleSubmit} ref={(fm) => this.myFormRef = fm}>
+                    <input type='text' placeholder='Enter new name' name='name' />
+                    <button type='submit'>Edit</button>
+                </form>
+            </div>
+        )
+    }
+}
+export default EditBtnMdl;
