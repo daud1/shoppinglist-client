6478ddfa5d18ca886eac06d4717b3cd7934c78a2
Daudi Mwebaza <david.mwebaza@andela.com>
2018-02-12 11:16:00 +0300

    [Feature#154399656] Fix Item & List View render tests.

--- react-client/src/setupTests.js
+++ react-client/src/setupTests.js
@@ -3,26 +3,26 @@ import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
 
-// class LocalStorageMock {
-//   constructor() {
-//     this.store = {};
-//   }
+class LocalStorageMock {
+  constructor() {
+    this.store = {};
+  }
 
-//   clear() {
-//     this.store = {};
-//   }
+  clear() {
+    this.store = {};
+  }
 
-//   getItem(key) {
-//     return this.store[key] || null;
-//   }
+  getItem(key) {
+    return this.store[key] || null;
+  }
 
-//   setItem(key, value) {
-//     this.store[key] = value.toString();
-//   }
+  setItem(key, value) {
+    this.store[key] = value.toString();
+  }
 
-//   removeItem(key) {
-//     delete this.store[key];
-//   }
-// }
+  removeItem(key) {
+    delete this.store[key];
+  }
+}
 
-// global.localStorage = new LocalStorageMock();
+global.localStorage = new LocalStorageMock();
