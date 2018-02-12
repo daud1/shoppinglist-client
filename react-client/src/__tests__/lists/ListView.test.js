1dd5014d9127e22098942c653b37c01a5b61dac6
Daudi Mwebaza <david.mwebaza@andela.com>
2018-02-12 11:16:00 +0300

    [Feature#154399656] Fix Item & List View render tests.

--- react-client/src/__tests__/lists/ListView.test.js
+++ react-client/src/__tests__/lists/ListView.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { shallow, render, mount } from 'enzyme';
 import ListView from '../../components/lists/ListView.component';
 import shallowToJson from 'enzyme-to-json';
+
 describe ('ListView Component', () => {
     it('renders without crashing', () => {
         const wrapper = shallow(<ListView />);
