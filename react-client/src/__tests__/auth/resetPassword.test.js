b4b53f43cf7a26022c245f14c795a4509d1937bb
Daudi Mwebaza <david.mwebaza@andela.com>
2018-02-13 09:56:48 +0300

    [Feature#154399656] Add successful mock api call tests

--- react-client/src/__tests__/auth/resetPassword.test.js
+++ react-client/src/__tests__/auth/resetPassword.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { shallow, render, mount } from 'enzyme';
 import ResetPassword from '../../components/auth/resetPassword.component';
 import shallowToJson from 'enzyme-to-json';
+import moxios from 'moxios';
 
 
 describe ('ResetPassword Component', () => {
@@ -13,5 +14,22 @@ describe ('ResetPassword Component', () => {
     it('matches snapshot', () => {
         const wrapper = shallow(<ResetPassword />);
         expect(shallowToJson(wrapper)).toMatchSnapshot();
+    });
+
+    it('handles successful axios calls properly', (done) => {
+        const wrapper = shallow(<ResetPassword />);
+
+        wrapper.find('input[name="new_password"]').simulate('change', { target: { value: 'pswd' } });
+        wrapper.find('input[name="confirm"]').simulate('change', { target: { value: 'pswd' } });
+        wrapper.find('form').simulate('submit', {preventDefault() {} });
+
+        moxios.stubRequest('http://localhost:5000/auth/reset-password', {
+            status: 200,
+            'MSG': 'Password successfully reset!'
+        });
+        moxios.wait(() => {
+            expect(wrapper.html()).toContain('Password successfully reset!');
+            done();
+        });
     });
 })
--- react-client/src/__tests__/lists/createList.test.js
+++ react-client/src/__tests__/lists/createList.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {shallow, render, mount } from 'enzyme';
 import shallowToJson from 'enzyme-to-json';
 import CreateList from '../../components/lists/createList.component';
+import moxios from 'moxios';
 
 describe ('CreateList Form Component', () => {
     it('renders without crashing', () => {
@@ -12,5 +13,21 @@ describe ('CreateList Form Component', () => {
     it('matches snapshot', () => {
         const wrapper = shallow(<CreateList />);
         expect(shallowToJson(wrapper)).toMatchSnapshot();
+    });
+
+    it('handles successful axios calls properly', (done) => {
+        const wrapper = shallow(<CreateList />);
+
+        wrapper.find('input[name="name"]').simulate('change', { target: { value: 'newList' } });
+        wrapper.find('form').simulate('submit', { preventDefault() {} });
+
+        moxios.stubRequest('http://localhost:5000/shoppinglists/', {
+            status: 201,
+            'MSG': 'List created!'
+        });
+        moxios.wait(() => {
+            expect(wrapper.html()).toContain('List created!');
+            done();
+        });
     });
 })
