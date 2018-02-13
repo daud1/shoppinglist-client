ffcfd37ae51d7c4506add13be03f3f85bacc044c
Daudi Mwebaza <david.mwebaza@andela.com>
2018-02-13 09:56:48 +0300

    [Feature#154399656] Add successful mock api call tests

--- react-client/src/__tests__/auth/forgotPassword.test.js
+++ react-client/src/__tests__/auth/forgotPassword.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { shallow, render, mount } from 'enzyme';
 import shallowToJson from 'enzyme-to-json';
+import moxios from 'moxios';
 
 import ForgotPassword from '../../components/auth/forgotPassword.component';
 
@@ -13,5 +14,21 @@ describe ('ForgottenPassword Component', () => {
     it('matches snapshot', () => {
         const wrapper = shallow(<ForgotPassword />);
         expect(shallowToJson(wrapper)).toMatchSnapshot();
+    });
+
+    it('handles successful axios calls properly', (done) => {
+        const wrapper = shallow(<ForgotPassword />);
+
+        wrapper.find('input[name="email"]').simulate('change', { target: { value: 'user@email.com' } });
+        wrapper.find('form').simulate('submit', { preventDefault() {} });
+
+        moxios.stubRequest('http://localhost:5000/auth/forgot-password', {
+            status: 200,
+            'MSG': 'Password reset link sent!'
+        });
+        moxios.wait(() => {
+            expect(wrapper.html()).toContain('Password reset link sent!');
+            done();
+        });
     });
 })
--- react-client/src/__tests__/auth/signUp.test.js
+++ react-client/src/__tests__/auth/signUp.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { shallow, render, mount } from 'enzyme';
 import shallowToJson from 'enzyme-to-json';
 import SignUp from '../../components/auth/signUp.component';
+import moxios from 'moxios';
 
 
 describe ('SignUp Component', () => {
@@ -13,5 +14,26 @@ describe ('SignUp Component', () => {
     it('matches snapshot', () => {
         const wrapper = shallow(<SignUp />);
         expect(shallowToJson(wrapper)).toMatchSnapshot();
+    });
+
+    it('handles successful axios calls properly', (done) => {
+        const wrapper = shallow(<SignUp />);
+
+        wrapper.find('input[name="email"]').simulate('change', { target: { value: 'user@email.com' } });
+        wrapper.find('input[name="password"]').simulate('change', { target: { value: 'password' } });
+        wrapper.find('input[name="confirm"]').simulate('change', { target: { value: 'password' } });
+        wrapper.find('form').simulate('submit', { preventDefault() {} });
+        
+        moxios.stubRequest('http://localhost:5000/auth/register', {
+            status: 201,
+            response: {
+                'MSG': 'User successfully created!',
+            }
+
+        });
+        moxios.wait(() => {
+            expect(wrapper.html()).toContain('User successfully created!');
+            done();
+        });
     });
 })
