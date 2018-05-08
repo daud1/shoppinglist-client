4b1ee5c0f8f8ac3365b1ab15a3243090c0def6bb
Daudi Mwebaza <david.mwebaza@andela.com>
2018-05-08 15:51:55 +0300

    [Chore] Linting

--- src/components/auth/logIn.component.js
+++ src/components/auth/logIn.component.js
@@ -1,78 +1,79 @@
-import React, { Component } from 'react';
-import baseURL from '../generic/base';
-import axios from 'axios';
-import Paper from 'material-ui/Paper';
-import TextField from 'material-ui/TextField';
-import RaisedButton from 'material-ui/RaisedButton';
-import { notify } from 'react-notify-toast';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import baseURL from "../generic/base";
+import axios from "axios";
+import Paper from "material-ui/Paper";
+import TextField from "material-ui/TextField";
+import RaisedButton from "material-ui/RaisedButton";
+import { notify } from "react-notify-toast";
+import { Link } from "react-router-dom";
 
 const paperStyle = {
-    marginLeft: 'auto',
-    marginRight: 'auto',
-    marginTop: 200,
-    padding: '1%',
-    height: 300,
-    textAlign: 'center'
-}
+  marginLeft: "auto",
+  marginRight: "auto",
+  marginTop: 200,
+  padding: "1%",
+  height: 300,
+  textAlign: "center"
+};
 const divStyle = {
-    paddingLeft: '35%',
-    paddingRight: '35%'    
-}
+  paddingLeft: "35%",
+  paddingRight: "35%"
+};
 class LogIn extends Component {
-    handleSubmit = (event) => {
-        event.preventDefault();
-        let url = baseURL + 'auth/login';
-        let form = new FormData(event.target);
+  handleSubmit = event => {
+    event.preventDefault();
+    let url = baseURL + "auth/login";
+    let form = new FormData(event.target);
 
-        axios.post(url, form)
-        .then(response => {
-            localStorage.setItem('token', response.data.token)
-            notify.show('Successfully Logged In', 'success');
-            this.props.history.push("/lists");            
-        })
-        .catch(error => {
-            if(error.response){
-                const { data, status } = error.response;
-                if (status) {
-                    notify.show(data.ERR, 'error');
-                }
-            }
-            notify.show('Oops! Connection Error.', 'error');
-        });
-    }
+    axios
+      .post(url, form)
+      .then(response => {
+        localStorage.setItem("token", response.data.token);
+        notify.show("Successfully Logged In", "success");
+        this.props.history.push("/lists");
+      })
+      .catch(error => {
+        if (error.response) {
+          const { data, status } = error.response;
+          if (status) {
+            notify.show(data.ERR, "error");
+          }
+        }
+        notify.show("Oops! Connection Error.", "error");
+      });
+  };
 
-    render() {
-        return (
-            <div style={divStyle}>
-                <Paper style={paperStyle} zDepth={3}>
-                    <form onSubmit={this.handleSubmit}>
-                        <br />
-                        <div>
-                            <TextField
-                                type='email'
-                                name = 'email'
-                                floatingLabelText='Email Address'
-                            />
-                        </div>
-                        <div>
-                            <TextField
-                                type='password'
-                                name = 'password'
-                                floatingLabelText='Password' 
-                            />
-                        </div>
-                        <br />
-                        <div>
-                            <RaisedButton type='submit' label='Log In' primary />
-                        </div>
-                        <br />
-                        <Link to={'/forgotpassword'}>Forgot Password?</Link>
-                    </form>
-                </Paper>
+  render() {
+    return (
+      <div style={divStyle}>
+        <Paper style={paperStyle} zDepth={3}>
+          <form onSubmit={this.handleSubmit}>
+            <br />
+            <div>
+              <TextField
+                type="email"
+                name="email"
+                floatingLabelText="Email Address"
+              />
             </div>
-        );
-    }
+            <div>
+              <TextField
+                type="password"
+                name="password"
+                floatingLabelText="Password"
+              />
+            </div>
+            <br />
+            <div>
+              <RaisedButton type="submit" label="Log In" primary />
+            </div>
+            <br />
+            <Link to={"/forgotpassword"}>Forgot Password?</Link>
+          </form>
+        </Paper>
+      </div>
+    );
+  }
 }
 
 export default LogIn;
